60a6d382555f1999b3bb94ddf6e417a9de63f54c
Tony Bierman <tonybierman@gmail.com>
2023-07-14 20:05:36 -0500

    Refactoring

---
+++ Bierman.UiToolkit/ViewModel/DetailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bierman.UiToolkit.ViewModel
+{
+    public class DetailViewModel : PresentableViewModel
+    {
+        public DetailViewModel(string formName) : base(formName) { }
+        protected override void SaveFormExecute()
+        {
+            if (Data != null)
+            {
+                Data.Validate();
+                if (Data.IsValid)
+                {
+                    base.SaveFormExecute();
+                }
+            }
+        }
+    }
+}
